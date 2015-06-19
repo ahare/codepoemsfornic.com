15d344b6be174c09b2f6f66465858575e5887ed3
Andrew Hare <andrew@may17.org>
2015-06-19 03:36:00 -0400

    Better this way.

--- poems/behold.rb
+++ poems/behold.rb
@@ -1,6 +1,12 @@
 class Nic
+  class << self
+    attr_accessor :beauty
+  end
   @beauty = 1_000_000
-  def self.behold
-    @beauty += 1_000_000
+end
+
+class Me
+  def self.behold(nic)
+    nic.beauty += 1_000_000
   end
 end
